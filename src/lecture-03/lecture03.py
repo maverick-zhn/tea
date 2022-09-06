2f7896a6e3a8e1e70e2ce44cb67f720ae7518767
Servio Palacios <servio@palacios.com>
2022-09-06 11:04:16 -0400

    [lecture03]

---
+++ src/lecture-03/lecture03.py
@@ -0,0 +1,64 @@
+# Tendencias e Innovación en Tecnología Agrícola
+# Autor: servio@palacios.com
+# Fecha: 2022.08.29
+# Editado: 2022.08.29
+
+# Introduccion a analisis de datos
+# Tipos de datos
+# print("Hola, mundo!")
+
+# Variables, Valores, y Expresiones
+# asignación de enteros a variables
+horas = 10
+minutos = 15
+
+# calculando el total de minutos
+# Se evalua la expresion
+# luego se asigna a la variable
+total = horas * 60 + minutos
+
+# desplegar total:
+print(total)
+
+# mostrar el tipo de datos total:
+print(type(total))
+
+# display exponenciacion:
+
+# otroCalculo = 2 ** 3 + (6-10)/10
+# print(otroCalculo)
+# print(type(otroCalculo))
+
+# operador módulo
+# sobrante = 7 % 3
+# print(sobrante)
+
+# Tendencias e Innovacion en Tecnologia Agricola (TEA)
+# Operadores (Números y Cadenas)
+primero = 10
+segundo = 15
+
+# calculando el total de minutos
+#print(primero + segundo)
+
+# utilizando cadenas
+# primero1 = "10"
+# segundo1 = "15"
+# mostrar la concatenacion:
+# print(primero1 + segundo1)
+
+
+# preguntando datos por medio del teclado -----> (input)
+nombre = input("¿Cuál es tu nombre? ")
+print("Su nombre es: " + nombre)
+
+
+# Ejemplos de nombres de variables utilizando pothole_case_naming
+# temp_celcius = 26.67
+# primer_nombre = "Juan"
+
+# Ejemplos de nombres de variables utilizando camelCase
+# clase = "TEA"
+# tempFarenheit = 80.0
+# primerNombre = "Juan"
+# apellido = "White"
