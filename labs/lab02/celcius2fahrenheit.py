5a45aa76f76e4f8932df3d29572a5a96be986a5a
Servio Palacios <servio@palacios.com>
2022-09-08 09:47:34 -0400

    [lab02 -fahrenheit2celcius]

---
+++ labs/lab02/celcius2fahrenheit.py
@@ -0,0 +1,6 @@
+# Tendencias e Innovacion en Tecnologia Agricola (TEA)
+# El float sirve de conversion de tipo, el usuario ingresa un valor y Python trata de convertirlo a float
+temperaturaCelcius = float(input("Cual es la temperatura en Celcius? "))
+temperatureFahrenheit = (9/5) * temperaturaCelcius + 32
+print(temperatureFahrenheit)
+# solo falta imprimir el tipo
