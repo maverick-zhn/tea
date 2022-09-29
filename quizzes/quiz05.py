c8f32adb4fa70f93296c08630cc90eb18ace2db3
Servio Palacios <servio@palacios.com>
2022-09-29 18:54:41 -0400

    [data-visualization] - [quiz05]

---
+++ quizzes/quiz05.py
@@ -0,0 +1,73 @@
+# Tendencias e Innovacion en Tecnologia Agricola (TEA)
+# Quiz 05
+# Funciones
+# Autor: servio@palacios.com
+# Fecha: 2022.09.28
+# Editado: 2022.09.28
+# =================================================================================
+# Disclaimer:
+# Este codigo contiene instrucciones de Python que tiene el fin de
+# enseñar a programar. De esta forma, los comentarios y contenido puede incluir
+# errores de ortografia ya que algunos de estos son simbolos especiales que pueden
+# causar algunos problemas al documentar, leer, o utilizar este codigo con
+# propósitos educativos.
+# Best practices: De esta forma se sugiere el uso de estandares de programacion
+# que incluyen la reduccion de la utilizacion de Mayusculas o caracteres especiales
+# que estan fuera del proposito del programa.
+# De forma similar, el código incluye instrucciones que ensenan temas de forma
+# progresiva, esto es, puede contener soluciones no optimas.
+# =================================================================================
+
+# n = 5
+# while n > 0:
+#     print(n)
+# print('All done')
+
+# tot = 0
+# for i in [5, 4, 3, 2, 1]:
+#     tot = tot + 1
+# print(tot)
+
+# friends = ['Joseph', 'Glenn', 'Sally']
+# for friend in friends:
+#     print('Happy New Year:',  friend)
+# print('Done!')
+
+
+# zork = 0
+# for thing in [9, 41, 12, 3, 74, 15]:
+#     zork = zork + thing
+# print('After', zork)
+
+# smallest_so_far = -1
+# for the_num in [9, 41, 12, 3, 74, 15]:
+#     if the_num < smallest_so_far:
+#         smallest_so_far = the_num
+# print(smallest_so_far)
+
+# n = 0
+# while n > 0:
+#     print('Lather')
+#     print('Rinse')
+# print('Dry off!')
+
+
+# str1 = "Hello"
+# str2 = 'there'
+# bob = str1 + str2
+# print(bob)
+
+# x = 'From marquard@uct.ac.za'
+# print(x[8])
+
+# print(x[14:17])
+
+# greet = 'Hello Bob'
+
+# print(greet.upper())
+
+data = 'From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008'
+pos = data.find('.')
+print(data[pos:pos+3])
+
+print(len('banana')*7)
